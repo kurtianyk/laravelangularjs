1bb3c4463d5d16d2eb0690b36404c98e59a3b6fb
Oleh Kurtianyk <o.kurtianyk@gmail.com>
2017-02-27 14:58:26 +0200

    mask

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('getContact', []).constant('API_URL', 'http://localhost:8000/api/');
+var app = angular.module('getContact', ['ngMask']).constant('API_URL', 'http://localhost:8000/api/');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -64,35 +64,35 @@
                   <div class="form-group">
                     <label class="col-sm-3 control-label">Contact Name</label>
                     <div class="col-sm-9">
-                      <input type="text" class="form-control" id="name" name="name" placeholder="Contact Name" value="@{{name}}" ng-model="contact.name" ng-required="true">
+                      <input type="text" class="form-control" id="name" name="name" placeholder="Contact Name" value="@{{name}}" ng-model="contact.name"  ng-required="true">
                       <span ng-show="frmContact.name.$invalid && frmContact.name.$touched">Contact Name field is required</span>
                     </div>
                   </div>
                   <div class="form-group">
                     <label class="col-sm-3 control-label">Contact Surname</label>
                     <div class="col-sm-9">
-                      <input type="text" class="form-control" id="surname" name="surname" placeholder="Contact Surname" value="@{{surname}}" ng-model="contact.surname" ng-required="true">
+                      <input type="text" class="form-control" id="surname" name="surname" placeholder="Contact Surname" value="@{{surname}}" ng-model="contact.surname"  ng-required="true">
                       <span ng-show="frmContact.surname.$invalid && frmContact.surname.$touched">Contact Surname field is required</span>
                     </div>
                   </div>
                   <div class="form-group">
                     <label class="col-sm-3 control-label">Contact Phone Number</label>
                     <div class="col-sm-9">
-                      <input type="text" class="form-control" id="phone_number" name="phone_number" placeholder="Contact Phone Number" value="@{{phone_number}}" ng-model="contact.phone_number" ng-required="true">
+                      <input type="text" class="form-control" id="phone_number" name="phone_number" placeholder="Contact Phone Number +380XXXXXXX" value="@{{phone_number}}" ng-model="contact.phone_number" mask="+999999999999"  ng-required="true">
                       <span ng-show="frmContact.phone_number.$invalid && frmContact.phone_number.$touched">Contact Phone Number field is required</span>
                     </div>
                   </div>
                   <div class="form-group">
                     <label class="col-sm-3 control-label">Contact Birth Day</label>
                     <div class="col-sm-9">
-                      <input type="text" class="form-control" id="birth_day" name="birth_day"  placeholder="Contact Birth Day: XXXX-XX-XX" value="@{{birth_day}}" ng-model="contact.birth_day" ng-required="true">
+                      <input type="text" class="form-control" id="birth_day" name="birth_day"  placeholder="Contact Birth Day: XXXX-XX-XX" value="@{{birth_day}}" ng-model="contact.birth_day"  mask="2999-19-39" ng-required="true">
                       <span ng-show="frmContact.birth_day.$invalid && frmContact.birth_day.$touched">Contact Birth Day field is required</span>
                     </div>
                   </div>
                   <div class="form-group">
                     <label class="col-sm-3 control-label">Contact Information</label>
                     <div class="col-sm-9">
-                      <input type="text" class="form-control" id="info" name="info" placeholder="Contact Information" value="@{{info}}" ng-model="contact.info" ng-required="true">
+                      <input type="text" class="form-control" id="info" name="info" placeholder="Contact Information" value="@{{info}}" ng-model="contact.info"  ng-required="true">
                       <span ng-show="frmContact.info.$invalid && frmContact.info.$touched">Contact Information field is required</span>
                     </div>
                   </div>
@@ -131,7 +131,7 @@
     <!-- Aangular Material load from CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.1/angular-material.min.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ngMask/3.1.1/ngMask.min.js"></script>
     <!-- Angular Application Scripts Load  -->
     <script src="{{ asset('angular/app.js') }}"></script>
     <script src="{{ asset('angular/controllers/ContactController.js') }}"></script>
